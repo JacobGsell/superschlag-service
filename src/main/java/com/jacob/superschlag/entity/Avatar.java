6fa923293eceafc1a1189c5fa8d3d6065e0f4cb0
JacobGsell <gsellja67901@th-nuernberg.de>
2022-04-15 18:34:38 +0200

    refactored namings and database configuration for now

--- src/main/java/com/jacob/superschlag/entity/Avatar.java
+++ src/main/java/com/jacob/superschlag/entity/Avatar.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 @Data
@@ -28,4 +29,7 @@ public class Avatar {
 
     @OneToOne
     private Item amulet;
+
+    @ManyToOne
+    private Job job;
 }
