cb9610a8c61c854b1057c8ce8d331cd4561b83ef
JacobGsell <gsellja67901@th-nuernberg.de>
2022-04-15 16:02:09 +0200

    initial commit

---
+++ src/main/java/com/jacob/superschlag/SuperschlagApplication.java
@@ -0,0 +1,12 @@
+package com.jacob.superschlag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperschlagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperschlagApplication.class, args);
+	}
+}
